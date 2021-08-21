cb5406519b6982efd10d2ada38386f6b95cfb79c
unknown <siamur01@gmail.com>
2021-08-21 13:56:10 +0600

    calculition total complete

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // memory ....
-
 function extraMemoryCost(Cost, memoryCost) {
    const costForMemory = document.getElementById(Cost + '-extra-cost');
    let costForMemoryText = costForMemory.innerText;
@@ -11,26 +10,43 @@ function extraMemoryCost(Cost, memoryCost) {
 
    costForMemory.innerText = costForMemoryText;
 
-   // best price
+   // // best price
    const bestPrice = document.getElementById('best-price');
    const bestPriceText = bestPrice.innerText;
    const bestPriceTextNumber = parseInt(bestPriceText);
+   // //memory
+   const extraMemoryCosst = document.getElementById('memory-extra-cost');
+   const extraMemoryCosstText = extraMemoryCosst.innerText;
+   const extraMemoryCosstTextNumber = parseInt(extraMemoryCosstText);
+   // //storage
+   const extrastorageCosst = document.getElementById('storage-extra-cost');
+   const extrastorageCosstText = extrastorageCosst.innerText;
+   const extrastorageCosstTextNumber = parseInt(extrastorageCosstText);
+   // //delivery
+   const extradeliveryCosst = document.getElementById('delivery-extra-cost');
+   const extradeliveryCosstText = extradeliveryCosst.innerText;
+   const extrasdeliveryCosstTextNumber = parseInt(extradeliveryCosstText);
 
-   //update total cost for extra memory
-   const totalCost = document.getElementById('total-price');
-   totalCost.innerText = bestPriceTextNumber + costForMemoryText;
-   return costForMemoryText;
+   // update total cost for extra memory
+   const totalCost = document.getElementById('total-extra-cost');
+   totalCost.innerText = bestPriceTextNumber + extrastorageCosstTextNumber + extrasdeliveryCosstTextNumber + extraMemoryCosstTextNumber;
 }
+
+
 /////////////// MEMORY///////////////
+
 document.getElementById('8GB-btn').addEventListener('click', function () {
    extraMemoryCost('memory', true);
+
 })
 document.getElementById('16GB-btn').addEventListener('click', function () {
    extraMemoryCost('memory', false);
+
 })
 ///////////////// STORAGE ///////////////
 document.getElementById('256GB-btn').addEventListener('click', function () {
    extraMemoryCost('storage', true);
+
 })
 document.getElementById('512GB-btn').addEventListener('click', function btn() {
    extraMemoryCost('storage', '512-SSD');
@@ -41,8 +57,9 @@ document.getElementById('1TB-btn').addEventListener('click', function btn() {
 //////////////DELIVERY ///////////
 document.getElementById('Aug-25-btn').addEventListener('click', function btn() {
    extraMemoryCost('delivery', true);
+
 })
+
 document.getElementById('Aug-21-btn').addEventListener('click', function btn() {
    extraMemoryCost('delivery', 'Aug-21');
 })
-
