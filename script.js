76b5f1fe3cd00587a0ab711a51b3f1640a155246
unknown <siamur01@gmail.com>
2021-08-21 14:26:43 +0600

    almost finished

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // memory ....
+
 function extraMemoryCost(Cost, memoryCost) {
    const costForMemory = document.getElementById(Cost + '-extra-cost');
    let costForMemoryText = costForMemory.innerText;
@@ -9,44 +10,39 @@ function extraMemoryCost(Cost, memoryCost) {
    else if (memoryCost == 'Aug-21') { console.log('nothing'); costForMemoryText = 20; }
 
    costForMemory.innerText = costForMemoryText;
-
-   // // best price
+   calculation();
+}
+// calculation total prices with function
+function calculationFunction(update) {
+   const updateCost = document.getElementById(update + '-extra-cost');
+   const extraCost = updateCost.innerText;
+   const extraCostNumber = parseInt(extraCost);
+   return extraCostNumber;
+}
+function calculation() {
+   // // prices...
    const bestPrice = document.getElementById('best-price');
    const bestPriceText = bestPrice.innerText;
    const bestPriceTextNumber = parseInt(bestPriceText);
-   // //memory
-   const extraMemoryCosst = document.getElementById('memory-extra-cost');
-   const extraMemoryCosstText = extraMemoryCosst.innerText;
-   const extraMemoryCosstTextNumber = parseInt(extraMemoryCosstText);
-   // //storage
-   const extrastorageCosst = document.getElementById('storage-extra-cost');
-   const extrastorageCosstText = extrastorageCosst.innerText;
-   const extrastorageCosstTextNumber = parseInt(extrastorageCosstText);
-   // //delivery
-   const extradeliveryCosst = document.getElementById('delivery-extra-cost');
-   const extradeliveryCosstText = extradeliveryCosst.innerText;
-   const extrasdeliveryCosstTextNumber = parseInt(extradeliveryCosstText);
 
-   // update total cost for extra memory
+   const extraCostForMemory = calculationFunction('memory');
+   const extraCostForStorage = calculationFunction('storage');
+   const extraCostForDelivery = calculationFunction('delivery');
+
+   // calculation total prices
    const totalCost = document.getElementById('total-extra-cost');
-   totalCost.innerText = bestPriceTextNumber + extrastorageCosstTextNumber + extrasdeliveryCosstTextNumber + extraMemoryCosstTextNumber;
+   totalCost.innerText = bestPriceTextNumber + extraCostForStorage + extraCostForDelivery + extraCostForMemory;
 }
-
-
 /////////////// MEMORY///////////////
-
 document.getElementById('8GB-btn').addEventListener('click', function () {
    extraMemoryCost('memory', true);
-
 })
 document.getElementById('16GB-btn').addEventListener('click', function () {
-   extraMemoryCost('memory', false);
-
+   extraMemoryCost('memory', false)
 })
 ///////////////// STORAGE ///////////////
 document.getElementById('256GB-btn').addEventListener('click', function () {
    extraMemoryCost('storage', true);
-
 })
 document.getElementById('512GB-btn').addEventListener('click', function btn() {
    extraMemoryCost('storage', '512-SSD');
@@ -57,9 +53,7 @@ document.getElementById('1TB-btn').addEventListener('click', function btn() {
 //////////////DELIVERY ///////////
 document.getElementById('Aug-25-btn').addEventListener('click', function btn() {
    extraMemoryCost('delivery', true);
-
 })
-
 document.getElementById('Aug-21-btn').addEventListener('click', function btn() {
    extraMemoryCost('delivery', 'Aug-21');
 })
